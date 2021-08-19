e3ec2c42f75ebcefc0b91d66540f36ba2bc3d4b4
truxxu <danielfetrujillo@gmail.com>
2021-08-19 16:38:25 -0500

    parses md files and fetches list at build time

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import Link from 'next/link'
 
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -21,6 +31,20 @@ export default function Home() {
           <a>this first post</a>
         </Link>
       </h1>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
